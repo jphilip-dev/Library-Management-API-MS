7ac3a332bfbf37f6d23abd7ef458878ca6d21ab7
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-06-12 23:49:22 +0800

    Add AdminController - Add AdminFacadeService to group user services

--- auth-service/src/main/java/com/jphilips/library/auth/dto/cqrs/command/AdminUpdateUserCommand.java
+++ auth-service/src/main/java/com/jphilips/library/auth/dto/cqrs/command/AdminUpdateUserCommand.java
@@ -1,7 +1,9 @@
 package com.jphilips.library.auth.dto.cqrs.command;
 
 import com.jphilips.library.auth.dto.AdminUpdateRequestDto;
+import lombok.Builder;
 
+@Builder
 public record AdminUpdateUserCommand(
         AdminUpdateRequestDto adminUpdateRequestDTO
 ) {
