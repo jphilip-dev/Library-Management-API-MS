c4da5ab325f0ad5b9d0323664ab421697d524aa7
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-06-19 09:29:18 +0800

    Add Query controller to retrieve branches - Refactor gateway routing to include branches

--- book-inventory-service/src/main/java/com/jphilips/library/bookinventory/controller/BranchAdminQueryController.java
+++ book-inventory-service/src/main/java/com/jphilips/library/bookinventory/controller/BranchAdminQueryController.java
@@ -1,4 +1,31 @@
 package com.jphilips.library.bookinventory.controller;
 
+import com.jphilips.library.bookinventory.dto.BranchResponseDto;
+import com.jphilips.library.bookinventory.service.branch.query.BranchQueryFacadeService;
+import com.jphilips.shared.dto.PagedResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/branch")
 public class BranchAdminQueryController {
+
+    private final BranchQueryFacadeService branchQueryFacadeService;
+
+    @GetMapping
+    public PagedResponse<BranchResponseDto> getAllBranch(Pageable pageable){
+        return branchQueryFacadeService.getAllBranch(pageable);
+    }
+
+    @GetMapping("/{code}")
+    public BranchResponseDto getBranchByCode(@PathVariable String code){
+        return branchQueryFacadeService.getBranchByCode(code);
+    }
+
 }
